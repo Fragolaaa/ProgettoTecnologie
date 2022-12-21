3b69150d2b87f775214db98d9215e4a762ea97d3
Tania Concil <99073493+Fragolaaa@users.noreply.github.com>
2022-12-21 11:49:26 +0100

    invio e ricezione in classe player lato client

--- UnoCardGame/Client/GameObjects/Player.cs
+++ UnoCardGame/Client/GameObjects/Player.cs
@@ -1,4 +1,37 @@
 //classe giocatore
 
 //to do: l'host deve ricevere i messaggi dal server, quando è il suo turno e poi comunicare al server la sua mossa
-//eventuali "regole infrante" come colore sbagliato non devono essere ammesse direttamente a livello del client.
+//eventuali "regole infrante" come colore sbagliato non devono essere ammesse direttamente a livello del client
+
+
+class Player(){
+    CardDeck hand = new CardDeck(); //l'utente ha un deck di carte da cui scegliere
+
+    Socket client = new( //socket per inviare poi i messaggi al server
+    ipEndPoint.AddressFamily, 
+    SocketType.Stream, 
+    ProtocolType.Tcp);
+
+    await client.ConnectAsync(ipEndPoint);
+
+    while (true)
+{
+    //Invio messaggio al server
+    var message = ""; //invio al server il nickname, la carta che ho deciso di buttare + eventuale bottone uno
+    var messageBytes = Encoding.UTF8.GetBytes(message);
+    _ = await client.SendAsync(messageBytes, SocketFlags.None);
+    Console.WriteLine($"*nickname*: \"{message}\"");
+
+    // ack
+    var buffer = new byte[1_024];
+    var received = await client.ReceiveAsync(buffer, SocketFlags.None);
+    var response = Encoding.UTF8.GetString(buffer, 0, received);
+    if (response == "<|ACK|>")
+    {
+        
+        break;
+    }
+    
+}
+
+}
