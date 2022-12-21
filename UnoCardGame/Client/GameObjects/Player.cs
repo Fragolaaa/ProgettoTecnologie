0bdf0c32fad7deb381c2eda505577e1e060a437d
Tania Concil <99073493+Fragolaaa@users.noreply.github.com>
2022-12-21 11:55:10 +0100

    modifiche alla classe player.cs lato client

--- UnoCardGame/Client/GameObjects/Player.cs
+++ UnoCardGame/Client/GameObjects/Player.cs
@@ -4,9 +4,10 @@
 //eventuali "regole infrante" come colore sbagliato non devono essere ammesse direttamente a livello del client
 
 
-class Player(){
+public class Player(){
+    String nickname;
     CardDeck hand = new CardDeck(); //l'utente ha un deck di carte da cui scegliere
-
+    
     Socket client = new( //socket per inviare poi i messaggi al server
     ipEndPoint.AddressFamily, 
     SocketType.Stream, 
@@ -14,14 +15,24 @@ class Player(){
 
     await client.ConnectAsync(ipEndPoint);
 
-    while (true)
-{
-    //Invio messaggio al server
+    public Player(String nickname, CardDeck hand){ //costruttore del giocatore
+        this.nickname=nickname;
+        this.hand=hand;
+    }
+    public String getNickname(){
+        return nickname;
+    }
+
+    public sendMessage(){
+        //Invio messaggio al server
     var message = ""; //invio al server il nickname, la carta che ho deciso di buttare + eventuale bottone uno
     var messageBytes = Encoding.UTF8.GetBytes(message);
     _ = await client.SendAsync(messageBytes, SocketFlags.None);
     Console.WriteLine($"*nickname*: \"{message}\"");
+    }
 
+    public receiveMessage(){
+            
     // ack
     var buffer = new byte[1_024];
     var received = await client.ReceiveAsync(buffer, SocketFlags.None);
@@ -31,7 +42,6 @@ class Player(){
         
         break;
     }
-    
-}
+    }
 
 }
