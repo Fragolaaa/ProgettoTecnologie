92f14350511fd814873f095a9a9adc177179bed9
Tania Concil <99073493+Fragolaaa@users.noreply.github.com>
2023-01-06 20:07:54 +0100

    notifica stato di gioco del giocatore

---
+++ UnoCardGame/Game/Notifies/NotifyPlayerState.java
@@ -0,0 +1,10 @@
+package Game.Notifies;
+
+import Game.PlayerState;
+
+public class NotifyPlayerState extends Notify{
+    public PlayerState playerState;
+}
+
+
+//stato di gioco del giocatore, è ancora attivo?
