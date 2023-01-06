69c8e3a2f647c91da51f277671eb3b69a713f50f
Tania Concil <99073493+Fragolaaa@users.noreply.github.com>
2023-01-06 20:00:15 +0100

    Notifica carta cambiata

---
+++ UnoCardGame/Game/Notifies/NotifyCardChanged.java
@@ -0,0 +1,11 @@
+package Game.Notifies;
+
+import Game.cards.Card;
+
+public class NotifyCardChanged extends Notify{
+    public Card card;
+    public int playerId;
+}
+
+
+//notifica all'utente che la carta in cima al mazzo è cambiata
