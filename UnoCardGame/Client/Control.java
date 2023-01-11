a446e4f315619257986695b1b446a83a2f676df4
more-cash <82999421+more-cash@users.noreply.github.com>
2023-01-11 12:06:12 +0100

    controllo colore

---
+++ UnoCardGame/Client/Control.java
@@ -0,0 +1,20 @@
+package Client;
+
+public abstract class Control extends Card{ 
+
+
+
+    public void sameColor(Card color){
+        public int colore;
+        if(colore == color) {
+            updateTheServer()
+        }
+    }
+
+    private void updateTheServer(Notifica notify){ 
+        server.sendNotify(notify);
+    }
+}
+
+
+
