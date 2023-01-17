61658fcf57407ac235d8002ca4bce444e03210b1
Tania Concil <99073493+Fragolaaa@users.noreply.github.com>
2023-01-17 16:53:31 +0100

    Modificata classe Game

--- UnoCardGame/Game/game.java
+++ UnoCardGame/Game/game.java
@@ -1,179 +1,83 @@
 package Game;
-
-import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-import Game.Cards.Card;
-import Game.Cards.CardVisitor;
-import Game.Cards.ChangeColorCard;
-import Game.Cards.ChangeColorCardDrawFour;
-import Game.Cards.NumberCard;
-import Game.Cards.WildCards;
-import Game.Actions.*;
-import Game.Notifies.*;
+import Game.Cards.*;
 
-public class Game implements Serializable{ 
-    private final Deck deck = new Deck();
+public class Game extends Thread{
+    private final int MAX_CARDS = 7;
+    private HashMap<String, Player> players = new HashMap<>();
+    private Deck deck = new Deck();
     private ArrayList<Card> downCards = new ArrayList<>();
-    private final int numberOfStartingCards = 7;
+    private boolean waiting = true;
+    private boolean playing = false;
 
-    private ArrayList<PlayerState> players= new ArrayList<>();
-    private Client currPlayer;
+    public Game(){ //game starts
 
-    public Game(ArrayList<Client> clients){
-        int id = 0;
-        for (Client client : clients) {
-            players.add(new PlayerState(client, id));
-            client.setGame(this);
-            // client.start();
-            id++;
-        }
     }
 
+    @Override
+    public void run() {
+        //waitForPlayers();
+        while(playing);
+    }
 
     public void start(){
-        //tutti i client nel game ricevono le proprie carte
-        //dai ad ogni player la sua carta (usa un for che toglie la prima carta del mazzo e la da al player)
-        for(int i = 0; i < numberOfStartingCards; i++){
-            for (PlayerState playerState : players) {
-                playerState.addCardToHand(deck.getTopCard());
-            }
+        downCards.addAll(deck.popCard(1)); //mette una carta sul tavolo per iniziare
+        for (Map.Entry<String, Player> player : players.entrySet()) {
+            player.getValue().addToHand(deck.popCard(MAX_CARDS)); //ogni giocatore pesca 7 carte
+            sendToPlayer(player.getValue().getClientSocket(), player.getValue().getHand());
         }
-
-        for (PlayerState playerState : players) {
-            playerState.notifySelfState();
-        }
-        downCards.add(deck.getTopCard());
-        //si dice chi inizia
-        nextPlayer(null, players);
-
+        playing = true;
     }
 
-    private Card getLastCard(){ //prendo l'ultima carta che ho buttato sul tavolo
-        return downCards.get(downCards.size()-1);
-    }
-
-    private boolean checkCards(Card card1, Card card2){ //controllo se è possibile mettere a terra la carta
-
-        //posso settare se: colore/numero uguale a quella prima o una cambio colore
-        if(card1.color == card2.color || card1 instanceof ChangeColorCard || card1 instanceof ChangeColorCardDrawFour){
-            //ok colore uguale/cambio colore
-            return true;
+    public String addPlayer(String ID, Client client, String name){
+        if(waiting) {
+            Player player = players.put(ID, new Player(client, name));
+            String message = player == null ? "OK;" + name + " joined the lobby" : "ERROR;" + name + " already in this lobby";
+            System.out.println(message);
+            return message;
+            //put torna null se la chiave non e' mai stata istanziata prima, altrimenti
+            //torna il valore precedente
         }
-        else if(card1.getClass() == card2.getClass()){
-            //controlla se numbercard
-
-            if(card1 instanceof NumberCard){
-                NumberCard nCard1 = (NumberCard)card1;
-                NumberCard nCard2 = (NumberCard)card2;
-                return nCard1.value == nCard2.value; 
-            }
-            else{
-                //ok
-                return true;
-            }
-    
-        }
-        else{
-            //not ok
-            return false;
-        }
-    }
-    
-    public boolean setCard(Client client, Card card) { //imposta la nuova carta
-        //controllo validità carta
-        Card lastCard = getLastCard();
-        if(checkCards(card, lastCard)){
-            card.accept(new CardVisitor(this, client));
-            
-            downCards.add(card); //l'ultima carta inserita e' sempre in posizione card.size() - 1
-            
-            NotifyPlayerHandChanged notifyPlayerHandChanged = new NotifyPlayerHandChanged();
-            notifyPlayerHandChanged.howManyCardsChanged = -1;
-            updateAllPlayers(notifyPlayerHandChanged, client);
-            NotifyCardChanged notifyCardChanged = new NotifyCardChanged();
-            notifyCardChanged.card = card;
-            updateAllPlayers(notifyCardChanged);
-            nextPlayer(client, players); //passo al prossimo giocatore
-            return true;
-            
-        }
-           
-        else{
-        
-            updatePlayer(new NotifyInvalidMove("Invalid card"), client);
-            return false;
-        //mandare la risposta al client se puo' o non puo' settare la carta
-        }
-            
-
-    }
-    //per gestire i turni quando in modalità antioraria basta che giro il vettore...
-    public void nextPlayer(Client cp,ArrayList<PlayerState> players) {
-        //prendo il giocatore attuale, vado al prossimo
-        int c=0;
-        if(cp != null){ //se ho già iniziato il gioco
-            for (PlayerState p : players) {
-                    if(p.client.equals(cp)){
-                        if(players.indexOf(p)==(players.size()-1))
-                            c=0;
-                        else
-                            c=players.indexOf(p); 
-                    } 
-            }
-        }
-
-        currPlayer=players.get(c).client;
-        //dico al prox giocatore che è il suo turno  (isPlaying del giocatore a true)
-        NotifyTurn notifyTurn = new NotifyTurn();
-        updatePlayer(notifyTurn, currPlayer);
-    
-    }
-
-    /**
-     * @param notify
-     * @param client the client not to update
-     */
-    
-    private void updateAllPlayers(Notify notify, Client client){ //updates all clients except one
-        for (PlayerState player : players) {
-            if(!player.client.equals(client)) player.client.sendNotify(notify);
-        }
-    }
-    private void updateAllPlayers(Notify notify){ //updates all clients
-        for (PlayerState player : players) {
-            player.client.sendNotify(notify);
+        else {
+            System.out.println("The lobby is full"); //potrebbe essere mandato come messaggio al client
+            return "ERROR;The lobby is full";
         }
     }
 
-    private void updatePlayer(Notify notify, Client client){ //updates a single player
-        client.sendNotify(notify);
+    private void waitForPlayers() {
+        WaitForPlayers wait = new WaitForPlayers(this);
+        wait.start();
+        try {
+            wait.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        waiting = false;
+        start();
     }
 
-    public void nextDraw(int i) { //faccio pescare n carte al giocatore
-        nextPlayer(currPlayer, players);
-        for (PlayerState p : players) {
-            if(p.client.equals(currPlayer)){
-                for(int j=0;j<=i;j++){
-                    p.addCardToHand(deck.getTopCard());
-                }
-            }
+    public int countPlayers() {
+        return players.size();
+    }
+
+    private void sendMessageBroadcast(Object message){
+        for (Map.Entry<String, Player> player : players.entrySet()) {
+            player.getValue().getClientSocket().sendMessageToClient(message); //ogni giocatore pesca 7 carte
         }
     }
 
-    public void reverse() { // rovescio l'arraylist
+    private void sendToPlayer(Client clientSocket, Object message) {
+        clientSocket.sendMessageToClient(message);
+    }
+
+    public void reverseCard(){
         for (int i = 0; i < players.size() / 2; i++) {
-            PlayerState temp = players.get(i);
+            Player temp = players.get(i);
             players.set(i, players.get(players.size() - i - 1));
             players.set(players.size() - i - 1, temp);
         }
-    }
-
-    public void skipPlayer() {
-        nextPlayer(currPlayer, players);
-        NotifyDeniedTurn notifyDeniedTurn = new NotifyDeniedTurn();
-        updatePlayer(notifyDeniedTurn, currPlayer);//the turn has been denied, player turn must be skipped
-
-        nextPlayer(currPlayer, players);//next player
     }
 }
