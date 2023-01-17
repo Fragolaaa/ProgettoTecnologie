a530a17e696e00a76481e3c7f7b87cb77a81fba4
Tania Concil <99073493+Fragolaaa@users.noreply.github.com>
2023-01-17 17:05:36 +0100

    Update Server.java

--- UnoCardGame/Game/Server.java
+++ UnoCardGame/Game/Server.java
@@ -1,69 +1,94 @@
 package Game;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+// import java.util.Arrays;
+// import java.util.List;
 
-import Game.Actions.*;
-import Game.Notifies.*;
+// import uno.server.cards.NumberCard;
+// import uno.server.cards.Card;
+// import uno.server.cards.colorcards.ColorChangerCard;
+// import uno.server.cards.colorcards.ColorChangerCardDrawFour;
+// import uno.server.cards.wildcards.WildCardDrawTwo;
+// import uno.server.cards.wildcards.WildCardReverse;
+// import uno.server.cards.wildcards.WildCardSkipTurn;
 
-public class Server {
+public class Server{
+    public static void main(String[] args){
+        int PORT = 12345;
+    
+        Server server = new Server(PORT);
+    
+        ServerSocket serverSocket = null;
+        
+        try {
+            serverSocket = new ServerSocket(PORT);
+        } catch (Exception e) {
+            System.err.println("Connection Failed");
+        }
+        //Deck deck = new Deck();
+        // ColorChangerCardDrawFour card = new ColorChangerCardDrawFour();
+        // System.out.println(card.drawCLI());
+        // card.use(1);
+        // System.out.println(card.drawCLI());
+        // System.out.print(new WildCardDrawTwo(0).drawCLI());
+        // System.out.print(new WildCardReverse(2).drawCLI());
+        // System.out.print(new WildCardSkipTurn(3).drawCLI());
+        // System.out.print(new ColorChangerCard().drawCLI());
+        // System.out.print(new NumberCard(0, 10).drawCLI());
+        // ArrayList<Card> cards = new ArrayList<Card>(Arrays.asList(
+        //     new WildCardDrawTwo(0),
+        //     new WildCardReverse(1),
+        //     new WildCardSkipTurn(2),
+        //     new NumberCard(3, 10),
+        //     new ColorChangerCard(),
+        //     new ColorChangerCardDrawFour(),
+        //     card
+        // ));
+        // System.out.println(Card.drawCLI(cards));
+        server.run(serverSocket);
+    }
 
-    public final int PORT;
-
-    public ArrayList<Client> clients = new ArrayList<>();
+    //private final int PORT;
+    private static ArrayList<Client> clients = new ArrayList<>();
 
     public Server(int PORT){
-        this.PORT = PORT;
+        //this.PORT = PORT;
     }
 
     public void run(ServerSocket serverSocket){
         try {
+            Game game = new Game();
             while(true){
-                Socket socketClient;
-                    
-                socketClient = serverSocket.accept();
-                System.out.println(socketClient + " joined the lobby");
-    
-                ObjectOutputStream out = new ObjectOutputStream(socketClient.getOutputStream());
-                ObjectInputStream in = new ObjectInputStream(socketClient.getInputStream());
-    
-                Client client = new Client(PORT, out, in, socketClient);
-    
+                Socket clientSocket;
+                
+                clientSocket = serverSocket.accept();
+                System.out.println("Client " + clientSocket + " is connected");
+
+                ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+                ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
+
+                Client client = new Client(out, in, clientSocket, game);
+
                 clients.add(client);
-    
+
                 client.start();
-
-                // if(clients.size() == 4){ //to do: chiedi al client quanti giocatori vuole
-                //     startGame();
-                // }
-                startGame();
             }
-        } catch (Exception e) {
+        }catch(IOException e){
             e.printStackTrace();
+        }finally{
+            try {
+                if(serverSocket != null){
+                    serverSocket.close();
+                    //blocca tutti i client
+                }
+            }catch(IOException e){
+                e.printStackTrace();
+            }
         }
-    }
-
-    private void startGame() {
-        //(ArrayList) clients.subList(0, 3)
-        Game game = new Game(clients);
-        game.start();
-    }
-
-    public static void main(String[] args) {
-        int port = 12345;
-        Server server = new Server(port);
-
-        ServerSocket serverSocket = null;
-        
-        try {
-            serverSocket = new ServerSocket(port);
-        } catch (Exception e) {
-            System.err.println("Connection Failed");
-        }
-        Deck deck = new Deck();
-        server.run(serverSocket);
     }
 }
